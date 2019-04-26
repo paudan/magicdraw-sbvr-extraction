49ffbaf94e34389dd13f93f1e538973e24bcf51f
Paulius <danpaulius@gmail.com>
2019-04-26 19:24:50 +0300

    Add stats extraction

--- tests/org/ktu/model2sbvr/BPMNExtractionExperimentTest.java
+++ tests/org/ktu/model2sbvr/BPMNExtractionExperimentTest.java
@@ -15,30 +15,62 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class BPMNExtractionExperimentTest extends BpmnExtractionTestCase {
 
+    private Map<String, Map<String, Integer>> ruleStats = new HashMap<>();
+    private Set<String> ruleNames;
+
+    public BPMNExtractionExperimentTest() {
+        ruleNames = new TreeSet<>();
+        for (int i = 1; i <=9; i++)
+            ruleNames.add("T" + i);
+        ruleNames.add("Complex");
+    }
+
     @Override
     protected Path getFilename() {
         return Paths.get("tests", "resources", "bpmn", "experiment_refactored.mdzip");
     }
 
-    private void saveExtractorOutput(Map<SourceEntry, ConceptExtractionEntry> objects, Writer writer) throws IOException {
+    private void saveExtractorOutput(Map<SourceEntry, ConceptExtractionEntry> objects, Writer writer, String modelName) throws IOException {
         int i = 1;
+        Map<String, Integer> modelStats = ruleStats.get(modelName);
         for(Entry<SourceEntry, ConceptExtractionEntry> item: objects.entrySet()) {
             List<String> outputs = new ArrayList<>();
             for (SBVRExpressionModel sbvr: item.getValue().getCandidates())
                 outputs.add(sbvr.toString());
-            writer.write(String.format("%d. [%s] Source: ", i, item.getKey().getRule()) +
-                    String.join(",", item.getKey().getSourceNames()) +
+            String rule = item.getKey().getRule();
+            modelStats.put(rule, modelStats.get(rule) + 1);
+            writer.write(String.format("%d. [%s] Source: ", i, rule) + String.join(",", item.getKey().getSourceNames()) +
                     " -> output: " + String.join(",", outputs) + "\r\n");
             i += 1;
+        }
+    }
+
+    private void outputStats(String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), StandardCharsets.UTF_8))) {
+            writer.write("Model;");
+            writer.write(String.join(";", ruleNames));
+            writer.newLine();
+            for (Entry<String, Map<String, Integer>> modelEntry: ruleStats.entrySet()) {
+                writer.write(modelEntry.getKey() + ";");
+                for (Entry<String, Integer> ruleEntry: modelEntry.getValue().entrySet())
+                    writer.write(ruleEntry.getValue() + ";");
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -55,12 +87,17 @@ public class BPMNExtractionExperimentTest extends BpmnExtractionTestCase {
                 "10a", "10b", "11a", "11b", "12a", "12b", "12c", "12d", "12e"};
         for (String modelName: modelNames) {
             BpmnSBVRExtractor extractor = getExtractor(modelName);
+            Map<String, Integer> modelStats = new TreeMap<>();
+            for (String ruleName: ruleNames)
+                modelStats.put(ruleName, 0);
+            ruleStats.put(modelName, modelStats);
             String filePath = Paths.get(dirName, modelName + ".txt").toString();
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filePath), StandardCharsets.UTF_8))) {
-                saveExtractorOutput(extractor.getBRCandidateModel().getDataset(), writer);
+                saveExtractorOutput(extractor.getBRCandidateModel().getDataset(), writer, modelName);
             } catch (IOException e) {
                 Logger.getLogger(getName()).log(Level.SEVERE, null, e);
             }
         }
+        outputStats(Paths.get(dirName,"general_stats.csv").toString());
     }
 }
